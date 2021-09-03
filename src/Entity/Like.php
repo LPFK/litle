d04c5b2e54dda5f24838392efdffb4c608b089d5
Liaenis <84380297+Liaenis@users.noreply.github.com>
2021-09-03 14:56:59 +0200

    Like

---
+++ src/Entity/Like.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LikeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LikeRepository::class)
+ * @ORM\Table(name="`like`")
+ */
+class Like
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+}
