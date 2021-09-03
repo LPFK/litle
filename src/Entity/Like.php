d7882664e98ca501c84b80ae5c20b46012219837
Liaenis <84380297+Liaenis@users.noreply.github.com>
2021-09-03 15:08:50 +0200

    modif like

--- src/Entity/Like.php
+++ src/Entity/Like.php
@@ -20,9 +20,27 @@ class Like
      */
     private $id;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=UtilisateurConnecte::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $idLikeur;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getIdLikeur(): ?UtilisateurConnecte
+    {
+        return $this->idLikeur;
+    }
+
+    public function setIdLikeur(?UtilisateurConnecte $idLikeur): self
+    {
+        $this->idLikeur = $idLikeur;
+
+        return $this;
     }
 
 }
